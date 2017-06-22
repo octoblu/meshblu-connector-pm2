a34b3b5685eef6ec0ec0c48bcc6d65a63cae0496
Jade Meskill <jade.meskill@gmail.com>
2017-06-22 15:48:11 -0700

    v1.3.11 parse .env files for meshblu-connector-pm2

--- pm2.js
+++ pm2.js
@@ -1,9 +1,12 @@
 const fs = require("fs")
 const path = require("path")
 const ini = require("ini")
+const dotenv = require("dotenv")
+const dotenvExpand = require("dotenv-expand")
 
 let execPath = path.dirname(process.argv[1])
 if (process.pkg) execPath = path.dirname(process.execPath)
+
 const envIniFile = path.join(execPath, 'env.ini')
 if (fs.existsSync(envIniFile)) {
   const parsedIni = ini.parse(fs.readFileSync(envIniFile, 'utf-8'))
@@ -11,6 +14,10 @@ if (fs.existsSync(envIniFile)) {
     process.env[key] = value
   })
 }
+
+const envFile = path.join(execPath, '.env')
+const parsedEnv = dotenv.config({ path: envFile })
+dotenvExpand(parsedEnv)
 
 if (!process.env.PM2_HOME && process.env.MESHBLU_CONNECTOR_PM2_HOME) {
   process.env.PM2_HOME = process.env.MESHBLU_CONNECTOR_PM2_HOME
