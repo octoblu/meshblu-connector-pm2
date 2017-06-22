758d4f0fa53049dbce40ab248339c3ee02ab3b78
Jade Meskill <jade.meskill@gmail.com>
2017-06-22 15:42:34 -0700

    v1.3.10 parse ini files

--- pm2.js
+++ pm2.js
@@ -1,3 +1,17 @@
+const fs = require("fs")
+const path = require("path")
+const ini = require("ini")
+
+let execPath = path.dirname(process.argv[1])
+if (process.pkg) execPath = path.dirname(process.execPath)
+const envIniFile = path.join(execPath, 'env.ini')
+if (fs.existsSync(envIniFile)) {
+  const parsedIni = ini.parse(fs.readFileSync(envIniFile, 'utf-8'))
+  forEach(parsedIni.environment, (value, key) => {
+    process.env[key] = value
+  })
+}
+
 if (!process.env.PM2_HOME && process.env.MESHBLU_CONNECTOR_PM2_HOME) {
   process.env.PM2_HOME = process.env.MESHBLU_CONNECTOR_PM2_HOME
 }
