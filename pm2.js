baf423fc19122604b60908e8a61d944e97f86cc9
Jade Meskill <jade.meskill@gmail.com>
2017-06-26 13:49:43 -0700

    v1.3.13 support foreach

--- pm2.js
+++ pm2.js
@@ -3,19 +3,20 @@ const path = require("path")
 const ini = require("ini")
 const dotenv = require("dotenv")
 const dotenvExpand = require("dotenv-expand")
+const forEach = require("lodash.foreach")
 
 let execPath = path.dirname(process.argv[1])
 if (process.pkg) execPath = path.dirname(process.execPath)
 
-const envIniFile = path.join(execPath, 'env.ini')
+const envIniFile = path.join(execPath, "env.ini")
 if (fs.existsSync(envIniFile)) {
-  const parsedIni = ini.parse(fs.readFileSync(envIniFile, 'utf-8'))
+  const parsedIni = ini.parse(fs.readFileSync(envIniFile, "utf-8"))
   forEach(parsedIni.environment, (value, key) => {
     process.env[key] = value
   })
 }
 
-const envFile = path.join(execPath, '.env')
+const envFile = path.join(execPath, ".env")
 const parsedEnv = dotenv.config({ path: envFile })
 dotenvExpand(parsedEnv)
 
