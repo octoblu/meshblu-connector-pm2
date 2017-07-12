409682634286dea88bfe5b50b24450f989457a75
Roy van de Water <r@octoblu.com>
2017-07-12 15:20:30 -0700

    v1.7.5 Add debconf support

---
+++ utils/build-for-arm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --rm --volume $PWD:/work \
+  --env-file .env \
+  octoblu/meshblu-connector-builder:linux-armv7 \
+    bash -c 'export HOME=/tmp/cache \
+      && meshblu-connector-pkger \
+      && meshblu-connector-installer-debian --destination-path=/usr/local/bin'
